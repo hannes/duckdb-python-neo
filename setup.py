bb622242d90df8ff293f2662389c73c2a974e18d
Hannes Mühleisen <hannes@muehleisen.org>
2025-04-09 16:51:45 +0200

    actually linking this stuff

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import zipfile
-import urllib.request
 import os
+import glob
+import zipfile
 import tempfile
-
-from setuptools import setup, Extension
+import setuptools
+import urllib.request
 
 libduckdb_url = 'https://github.com/duckdb/duckdb/releases/download/v1.2.2/static-libs-osx-arm64.zip'
 temp_zip_file = tempfile.mktemp()
@@ -19,18 +19,19 @@ temp_dir = tempfile.TemporaryDirectory()
 zf = zipfile.ZipFile(temp_zip_file, "r")
 zf.extractall(temp_dir.name)
 
+duckdb_static_libs = [os.path.join(temp_dir.name,  x) for x in glob.glob("*.a", root_dir=temp_dir.name)]
 
-setup(
-    ext_modules=[Extension(
+setuptools.setup(
+    ext_modules=[setuptools.Extension(
         'duckdb.native',
         include_dirs=[temp_dir.name],
         sources=['src/native/init.cpp'],
         extra_compile_args=[],
         extra_link_args=[],
-        libraries=[],
-        library_dirs=[],
+        extra_objects=duckdb_static_libs,
         language='c++',
         define_macros=[],
+
         py_limited_api = True
     )],
     options={'bdist_wheel': {'py_limited_api': 'cp310'}},
