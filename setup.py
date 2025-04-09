dda29f1b0746ae23c9765023b1c5152d840c3f2c
Hannes Mühleisen <hannes@muehleisen.org>
2025-04-09 16:55:08 +0200

    minor cleanup

--- setup.py
+++ setup.py
@@ -8,17 +8,15 @@ import tempfile
 import setuptools
 import urllib.request
 
+# TODO figure out platform
 libduckdb_url = 'https://github.com/duckdb/duckdb/releases/download/v1.2.2/static-libs-osx-arm64.zip'
+
 temp_zip_file = tempfile.mktemp()
-
 urllib.request.urlretrieve(libduckdb_url, temp_zip_file)
-
-
-temp_dir = tempfile.TemporaryDirectory()
-
 zf = zipfile.ZipFile(temp_zip_file, "r")
+temp_dir = tempfile.TemporaryDirectory()
 zf.extractall(temp_dir.name)
-
+zf.close()
 duckdb_static_libs = [os.path.join(temp_dir.name,  x) for x in glob.glob("*.a", root_dir=temp_dir.name)]
 
 setuptools.setup(
@@ -26,7 +24,7 @@ setuptools.setup(
         'duckdb.native',
         include_dirs=[temp_dir.name],
         sources=['src/native/init.cpp'],
-        extra_compile_args=[],
+        extra_compile_args=['-std=c++11'],
         extra_link_args=[],
         extra_objects=duckdb_static_libs,
         language='c++',
--- src/native/init.cpp
+++ src/native/init.cpp
@@ -1,4 +1,4 @@
-#define Py_LIMITED_API 0x030A0000 // this needs to correspond with entry in pyproject.toml
+#define Py_LIMITED_API 0x030A0000 // this needs to correspond with entry in setup.py
 #include "Python.h"
 #define DUCKDB_API_NO_DEPRECATED
 #include "duckdb.h"
